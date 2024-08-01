0cd7681c5e929aaf3ecd51a18deedc65714268d8
Girardi <girardimatheus27@gmail.com>
2024-08-01 11:56:34 -0300

    feat: Implementing Routes

---
+++ src/@types/fastify.d.ts
@@ -0,0 +1,14 @@
+import 'fastify'
+
+declare module 'fastify' {
+  export interface FastifyRequest {
+    user?: {
+      id: string
+      session_id: string
+      name: string
+      email: string
+      created_at: string
+      updated_at: string
+    }
+  }
+}
